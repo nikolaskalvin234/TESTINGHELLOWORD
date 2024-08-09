165e1f4c2f2b2e48d955184f227aa696f5a7cc77
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 22:57:06 +0700

    first commit

---
+++ TBO.py
@@ -0,0 +1,6 @@
+print("hellooo word")
+print("Jadwal Kuliah")
+print("SENIN : PEMROGRAMAN 3")
+print("SELASA : PRAK PEMROGRAMAN 3")
+print("KAMIS : KEWARGANEGARAAN 3")
+print("JUMAT: LOGIKA ANALISA DAN NUMERIKA 3")
